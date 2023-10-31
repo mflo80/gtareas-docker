d4d2725345cf4771a26fe4d3c9648124e6df62b0
Marcelo Florio <mflo80@gmail.com>
2023-10-31 11:40:21 -0300

    Se realizan mejoras

--- docker-run.sh
+++ docker-run.sh
@@ -2,9 +2,36 @@
 
 FILE="docker-compose.yml"
 
-docker network create --driver=bridge --subnet=192.168.1.0/24 gtnet
+if ping -c 1 -t 100 192.168.66.1; then
+	echo La RED gtnet ya se encuentra creada
+else
+	docker network create --driver=bridge --subnet=192.168.66.0/24 gtnet
+fi
+
+if [ ! -d "gtapi" ]; then
+	mkdir gtapi
+fi
+
+if [ ! -d "gtdb" ]; then
+	mkdir gtdb
+fi
+
+if [ ! -d "gtfrontend" ]; then
+	mkdir gtfrontend
+fi
+
+if [ ! -d "gtlogin" ]; then
+	mkdir gtlogin
+fi
+
+if [ ! -d "gtoauth" ]; then
+	mkdir gtoauth
+fi
 
 if [ -f "$FILE" ];  then
     docker compose up -d
-    docker exec -d gtoauth php artisan schedule:run >> /dev/null 2>&1
+	wait
+	if ping -c 1 -t 100 192.168.66.5; then
+		docker exec -d gtoauth php artisan schedule:run >> /dev/null 2>&1
+	fi
 fi
