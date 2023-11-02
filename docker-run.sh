fa681d45f84bbcf2af82882f612f78c903f89f55
Marcelo Florio <mflo80@gmail.com>
2023-11-02 20:09:45 -0300

    Se actualiza script

--- docker-run.sh
+++ docker-run.sh
@@ -2,7 +2,6 @@
 FILE=docker-compose.yml
 
 gtoauth_uno() {
-	docker exec -ti gtoauth composer install &&
 	docker exec -ti gtoauth php artisan key:generate &&
 	docker exec -ti gtoauth php artisan migrate:fresh --seed
 }
@@ -10,11 +9,10 @@ gtoauth_uno() {
 gtoauth_dos() {
 	docker exec -ti gtoauth php artisan passport:keys 
 	docker exec -ti gtoauth php artisan passport:client --password --no-interaction --name="gtareas"
-    	docker exec -ti gtoauth php artisan passport:client --personal --no-interaction --name="gtareas"
+	docker exec -ti gtoauth php artisan passport:client --personal --no-interaction --name="gtareas"
 }
 
 gtapi_uno() {
-	docker exec -ti gtapi composer install &&
 	docker exec -ti gtapi php artisan key:generate &&
 	docker exec -ti gtapi php artisan migrate --seed
 }
@@ -50,7 +48,15 @@ if [ -f "$FILE" ]; then
 	wait
 	if ping -c 1 -t 100 192.168.66.5; then
 		if [ ! -f gtareas-oauth/.env ]; then
-			cp gtareas-oauth/.env.example gtareas-oauth/.env
+			echo Cambiando a directorio gtareas-oauth
+			cd gtareas-oauth
+			echo Creando .env
+			cp .env.example .env
+			echo Actualizando composer
+			composer update
+			echo Cambiando a directorio raíz
+			cd ..
+			echo Ejecutando funciones
 			gtoauth_uno &&
 			gtoauth_dos
 		else
@@ -61,7 +67,15 @@ if [ -f "$FILE" ]; then
 
 	if ping -c 1 -t 100 192.168.66.6; then
 		if [ ! -f gtareas-api/.env ]; then
-			cp gtareas-api/.env.example gtareas-api/.env
+			echo Cambiando a directorio gtareas-api
+			cd gtareas-api
+			echo Creando .env
+			cp .env.example .env
+			echo Actualizando composer
+			composer update
+			echo Cambiando a directorio raíz
+			cd ..
+			echo Ejecutando funciones
 			gtapi_uno
 		fi
 	fi
